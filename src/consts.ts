671d6b860ed74081b52b135d038168ea85068e1e
Kyle Ross <kylejamesross@gmail.com>
2023-09-17 15:54:38 -0600

    Pull in portfolio JSON and finish up work history and projects pages

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Portfolio | Kyle Ross';
 export const SITE_DESCRIPTION = 'Welcome to Kyle Ross\'s Portfolio: Web Developer';
+export const portfolioJSON = await fetch('https://gitconnected.com/v1/portfolio/kylejamesross').then((response) => response.json());
