e287ad91f523e7978729f0a9942e5c9bf6685044
Kyle Ross <kylejamesross@gmail.com>
2023-09-18 21:35:55 -0600

    Setup google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://kylejamesross.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ]
 });
