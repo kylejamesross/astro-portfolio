5969e608859dbe49d1444679422520d6e2da48f0
Kyle Ross <kylejamesross@gmail.com>
2023-10-01 12:49:54 -0600

    Fix with lsp support and add json resume schema fix type errors

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import { jsonResumeSchema } from './jsonResumeSchema';
 
 export const SITE_TITLE = 'Portfolio | Kyle Ross';
 export const SITE_DESCRIPTION = 'Welcome to Kyle Ross\'s Portfolio: Web Developer';
-export const portfolioJSON = await fetch('https://gitconnected.com/v1/portfolio/kylejamesross').then((response) => response.json());
+export const portfolioJSON = await fetch('https://gitconnected.com/v1/portfolio/kylejamesross').then((response) => response.json()).then((json) => jsonResumeSchema.parse(json));
