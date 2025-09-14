62d9397a603fbb89e6270a4f6c8ba96b23490fdf
Kyle Ross <github@kylejamesross.com>
2025-09-14 15:39:40 -0600

    Update astro and portfolio schema

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Portfolio";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default =
+      pkgs.mkShell
+      {
+        packages = with pkgs; [
+          nodejs_22
+        ];
+      };
+  };
+}
